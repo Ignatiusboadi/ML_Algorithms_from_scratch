ee89d82a3a51a9281ed625e95f5b3d70db4b2127
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-11-22 23:50:20 +0000

    added predict methods, and a test file

---
+++ Naive_Bayes_algorithm/tests.py
@@ -0,0 +1,9 @@
+from sklearn import preprocessing, datasets
+from sklearn.model_selection import train_test_split
+from naive_bayes import NaiveBayes
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
