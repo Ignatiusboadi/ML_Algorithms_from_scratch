ce35e4812b827a22cbee7881254dabbea7bb85c8
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-11-22 23:56:50 +0000

    added codes to test for Naive Bayes code

--- Naive_Bayes_algorithm/tests.py
+++ Naive_Bayes_algorithm/tests.py
@@ -7,3 +7,9 @@ X, y = iris.data, iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+iris_nb = NaiveBayes()
+iris_nb.fit(X_train, y_train)
+
+predictions = iris_nb.predict(X_test)
+
+print('Naive Bayes classification accuracy', iris_nb.check_accuracy(y_test, predictions))
