d66608883c59fe51a9f9fa4abdf0e539836fd40d
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-11-22 00:15:57 +0000

    added regression example to tests.py

--- KNN_algorithm/tests.py
+++ KNN_algorithm/tests.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn import datasets
+from sklearn import datasets, preprocessing
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
@@ -7,9 +7,19 @@ X, y = iris.data, iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 from knn import KNN
-clf = KNN(k=5, task='classification')
-clf.fit(X_train, y_train)
-predictions = clf.predict(X_test)
-print(clf.check_accuracy(predictions, y_test))
+iris_clf = KNN(k=5, task='classification')
+iris_clf.fit(X_train, y_train)
+predictions = iris_clf.predict(X_test)
+print(iris_clf.assess_model(predictions, y_test))
 
-print((predictions == y_test).sum())
+housing = datasets.fetch_california_housing()
+H_train, H_test, p_train, p_test = train_test_split(housing.data, housing.target, test_size=0.8, random_state=32)
+
+scaler = preprocessing.StandardScaler()
+H_train = scaler.fit_transform(H_train)
+H_test = scaler.transform(H_test)
+
+cali_reg = KNN(k=2, task='regression')
+cali_reg.fit(H_train, p_train)
+predictions = cali_reg.predict(H_test)
+print(cali_reg.assess_model(predictions, p_test))
